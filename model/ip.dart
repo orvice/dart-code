55fc13f9becd19babe27efb2b390cbaf460d4c33
orvice L <orvice@gmail.com>
2022-03-06 02:58:13 +0800

    add ip model

---
+++ model/ip.dart
@@ -0,0 +1,20 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'ip.g.dart';
+
+@JsonSerializable()
+class IP {
+  @JsonKey(name: 'ip')
+  String ip;
+
+  String city;
+
+  IP(this.ip,this.city)
+
+  /// Connect the generated [_$IPFromJson] function to the `fromJson`
+  /// factory.
+  factory IP.fromJson(Map<String, dynamic> json) => _$IPFromJson(json);
+
+  /// Connect the generated [_$IPToJson] function to the `toJson` method.
+  Map<String, dynamic> toJson() => _$IPToJson(this);
+}
